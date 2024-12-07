f41a17e45f1bbd5256c15a6a29368152dd0d7283
Joan Ripoll <Joan-rv@users.noreply.github.com>
2024-12-07 13:59:28 +0100

    Solve day 7 first part

---
+++ 7/solution1.c
@@ -0,0 +1,76 @@
+#include <fcntl.h>
+#include <libgnl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef unsigned long long ull;
+
+ull string_to_ull(char** n) {
+    ull r = 0;
+    while ('0' <= **n && **n <= '9') {
+        r = r * 10 + **n - '0';
+        (*n)++;
+    }
+    return r;
+}
+
+ull pow_i(ull b, ull e) {
+    ull r = 1;
+    for (ull i = 0; i < e; i++) {
+        r *= b;
+    }
+    return r;
+}
+
+ull eval_eq(ull* n, size_t n_s, ull op) {
+    ull r = n[0];
+    for (size_t i = 1; i < n_s; i++) {
+        if (op & (1 << (i - 1))) {
+            r *= n[i];
+        } else {
+            r += n[i];
+        }
+    }
+    return r;
+}
+
+bool can_be_solved(ull t, ull* n, size_t n_s) {
+    for (ull op = 0; op < pow_i(2, n_s) - 1; op++) {
+        if (eval_eq(n, n_s, op) == t) {
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    int input = open("input", O_RDONLY);
+    if (input < 0) {
+        perror("open");
+    }
+    char* line;
+    unsigned long long sum = 0;
+    ull* numbers = NULL;
+    size_t numbers_size = 0;
+    while ((line = get_next_line(input))) {
+        char* saved = line;
+        ull target = string_to_ull(&line);
+        line++;
+        while (*(line++) == ' ') {
+            ull n = string_to_ull(&line);
+            numbers_size++;
+            numbers = realloc(numbers, numbers_size * sizeof(ull));
+            numbers[numbers_size - 1] = n;
+        }
+        if (can_be_solved(target, numbers, numbers_size)) {
+            sum += target;
+        }
+        numbers_size = 0;
+        free(numbers);
+        numbers = NULL;
+        free(saved);
+    }
+    printf("Total calibration result: %llu\n", sum);
+}
